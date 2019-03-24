4fa6857a2ed88a31385eac0a2ac4502332aabdc9
MrGregor228 <39625405+MrGregor228@users.noreply.github.com>
2019-03-24 18:57:52 +0300

    commit

--- js/scroll.js
+++ js/scroll.js
@@ -20,4 +20,10 @@
          
         $('body,html').animate({scrollTop: top}, 1000);
     });
+    $('.nav-link').click(function() {
+        var sectionTo = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(sectionTo).offset().top
+        }, 1500);
+    });
 });
